24bac1c6348b54dcb83e62901a2af4ea02004209
Willder Azevedo <willderazevedo434@gmail.com>
2022-09-21 17:36:57 -0300

    feat(captions): captions

--- resources/js/components/programmations/ProgrammationCaption.vue
+++ resources/js/components/programmations/ProgrammationCaption.vue
@@ -1,28 +1,34 @@
 <template>
   <div id="programmation-caption-component">
-    <div class="row" v-if="categories.length">
-      <div class="col-md-12">
-        <h5>Legendas de Categoria</h5>
-        <div class="row caption-wrapper">
-          <div class="col-md-3 caption-item" v-for="category in categories" :key="category.id">
-            <div class="color-preview mr-3" v-bind:style="{backgroundColor: category.color}"></div>
-            {{ category.name }}
+    <v-expansion-panels>
+      <v-expansion-panel>
+        <v-expansion-panel-header>
+          <h5 class="mb-0">Legendas de Categoria</h5>
+        </v-expansion-panel-header>
+        <v-expansion-panel-content>
+          <div class="row caption-wrapper">
+            <div class="col-md-3 caption-item" v-for="category in categories" :key="category.id">
+              <div class="color-preview mr-3" v-bind:style="{backgroundColor: category.color}"></div>
+              {{ category.name }}
+            </div>
           </div>
-        </div>
-      </div>
-    </div>
+        </v-expansion-panel-content>
+      </v-expansion-panel>
 
-    <div class="row" v-if="spaces.length">
-      <div class="col-md-12">
-        <h5>Legendas de Espaços</h5>
-        <div class="row caption-wrapper">
-          <div class="col-md-3 caption-item" v-for="space in spaces" :key="space.id" v-bind:style="{'--icon-filter': iconFilter}">
-            <img :src="space.icon_url" class="mr-3" :alt="`${space.name} Icon`" width="30px" height="30px">
-            {{ space.name }}
+      <v-expansion-panel>
+        <v-expansion-panel-header>
+          <h5 class="mb-0">Legendas de Espaços</h5>
+        </v-expansion-panel-header>
+        <v-expansion-panel-content>
+          <div class="row caption-wrapper">
+            <div class="col-md-3 caption-item" v-for="space in spaces" :key="space.id" v-bind:style="{'--icon-filter': iconFilter}">
+              <img :src="space.icon_url" class="mr-3" :alt="`${space.name} Icon`" width="30px" height="30px">
+              {{ space.name }}
+            </div>
           </div>
-        </div>
-      </div>
-    </div>
+        </v-expansion-panel-content>
+      </v-expansion-panel>
+    </v-expansion-panels>
   </div>
 </template>
 
