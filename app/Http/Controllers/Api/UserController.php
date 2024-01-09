ad0c9687d5a48d115da3689d4e61600d35f46ee3
Willder Azevedo <willderazevedo434@gmail.com>
2024-01-09 14:58:31 -0300

    fix users list

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -22,6 +22,7 @@ class UserController extends Controller
             if ($request->role->tag === 'responsible') $users->doesntHave('hasSector');
         }
 
+        if ($request->has('active') && $request->active == 1) $users->where('active', true);
         if ($authUser->role->tag !== 'administrator') $users->whereHas('role', fn ($query) => $query->where('tag', '<>', 'administrator'));
         if ($authUser->role->tag === 'responsible') $users->where('sector_id', auth()->user()->sector->id)->where('id', '<>', $authUser->id);
 
