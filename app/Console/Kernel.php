7e4ef7f4bcd47b0bbd23a67c724b71e0982f8268
Willder Azevedo <willderazevedo434@gmail.com>
2024-01-22 14:08:42 -0300

    job to delete old solicitations

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Jobs\DeleteOldSolicitations;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 use App\Jobs\DispatchProgrammationReminders;
@@ -26,6 +27,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->job(new DispatchProgrammationReminders)->dailyAt('07:00');
+        $schedule->job(new DeleteOldSolicitations)->dailyAt('7:00');
     }
 
     /**
